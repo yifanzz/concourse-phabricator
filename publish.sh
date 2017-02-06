a133ea1807c36aa357ef1ef78290d80c09fd829d
yifan <me@yifan.co>
2017-02-06 23:34:48 +0000

    Added placeholder for resource builds

--- publish.sh
+++ publish.sh
@@ -2,12 +2,32 @@
 
 set -ex
 
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+export CGO_ENABLED=0 
+export GOOS=linux 
+export GOARCH=amd64 
+
 pushd companion
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+mkdir build-out
+go build -o build-out/main main.go
 export REPO=yifanzz/concourse-phabricator-companion
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker build -f Dockerfile -t $REPO:$COMMIT .
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO
+
+popd
+
+pushd resource
+
+mkdir build-out
+
+go build -o build-out/in in/main.go
+go build -o build-out/out out/main.go
+go build -o build-out/check check/main.go
+export REPO=yifanzz/concourse-phabricator-resource
 docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
